e4caab6694502118719575b1f4b19dbf707a5a5c
arongi2436 <tyroh@hyundai-ngv.com>
2022-04-21 15:27:17 +0900

    1.02

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 from setuptools import setup
 setup(
     name="TY_git_TEST",
-    version='1.0.1',
+    version='1.0.2',
     description="TY_test",
     url='https://github.com/arongi2436/TY_git_TEST.git',
     author='Taeyeoun Roh',
     author_email='tyroh@hyundai-ngv.com',
     packages=['TY_git_TEST'],
-    include_package_data=False,
+    include_package_data=True,
+    package_data={
+        '': ['file_test/test_file.xlsx'
+             ]},
     zip_safe=False,
     install_requires=['pandas>=0.24.0']
 )
