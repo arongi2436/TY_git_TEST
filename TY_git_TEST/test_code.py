b0e45b6797df013bc5903e1b22f9cb739dd4e05d
arongi2436 <tyroh@hyundai-ngv.com>
2022-04-21 15:28:01 +0900

    Update test_code.py

--- TY_git_TEST/test_code.py
+++ TY_git_TEST/test_code.py
@@ -2,3 +2,7 @@ import pandas as pd
 
 def input_test():
     print(pd.DataFrame({"TEST":["테스트"],"TEST2":["완료"]}))
+
+def read_data():
+    a=pd.read_excel('./file_test/test_file.xlsx',keep_default_na=False)
+    print(a)
